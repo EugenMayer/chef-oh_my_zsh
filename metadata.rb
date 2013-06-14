ca9687d607dc52dad31b9bf56802ff797391ac58
Vasily Mikhaylichenko <vasily.mikhaylichenko@gmail.com>
2013-06-15 03:29:25 +1000

    List supported platforms.

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,8 @@ maintainer_email 'vaskas@lxmx.com.au'
 license          'BSD'
 description      'A chef cookbook to install and configure oh-my-zsh'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.4.0'
+version          '0.4.1'
+
+%w{ubuntu centos redhat gentoo}.each { |p| supports p }
 
 %w{user ark zsh}.each { |c| depends c }
